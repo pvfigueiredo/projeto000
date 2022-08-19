136a5660a7ec607d4ce4dd42e6d0a8c5efe2c719
pvfigueiredo <paulo.figueiredo86@gmail.com>
2022-08-18 21:48:13 -0300

    interface para Cliente

---
+++ Webservice/Model/ICliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webservice.Model
+{
+    interface ICliente
+    {
+        public int ClienteID { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
