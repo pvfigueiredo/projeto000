c10650961ead4c2329af0e6657fc5ba539d2f9f4
pvfigueiredo <paulo.figueiredo86@gmail.com>
2022-08-20 18:45:32 -0300

    criado controller

--- Webservice/Model/Cliente.cs
+++ Webservice/Model/Cliente.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Webservice.Model
 {
-    interface ICliente
+    public class Cliente
     {
-        public int ClienteID { get; set; }
+        public Guid ClienteID { get; set; }
         public string Nome { get; set; }
         public string SobreNome { get; set; }
         public string CPF { get; set; }
---
+++ Webservice/Repositories/ClientesRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using Webservice.Model;
+
+namespace Webservice.Repositories
+{
+    public class ClientesRepository
+    {
+    }
+}
