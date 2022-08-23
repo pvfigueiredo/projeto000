304b8389e0352009c54f471f5f675279ecd9bd26
Arthur de Sousa Lima <106205450+Arthurlima100@users.noreply.github.com>
2022-08-23 19:24:12 -0300

    Método Put

---
+++ Webservice/WebService/Controllers/ClienteController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Webservice.Entities;
+using Webservice.Repositories;
+
+namespace Webservice.Controllers
+{
+    [ApiController]
+    [Route("clientes")]
+    public class ClienteController : ControllerBase
+    {
+        ClientesRepository _clienteRepository;
+        public ClienteController()
+        {
+            _clienteRepository = new ClientesRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Cliente> GetClientes()
+        {
+            return _clienteRepository.GetClientes();
+        }
+        
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Cliente> GetCliente(Guid id)
+        {
+            var cliente = _clienteRepository.GetCliente(id);
+            
+            if(cliente == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cliente);
+        }
+
+        [HttpPost]
+        public ActionResult PostCliente(Cliente request)
+        {
+            if(request == null)
+            {
+                return BadRequest();
+            }
+            var cliente = new Cliente() { ClienteId = Guid.NewGuid(),Nome = request.Nome,Sobrenome = request.Sobrenome,CPF = request.CPF,DataNascimento = request.DataNascimento,Email = request.Email };
+            _clienteRepository.SaveCliente(cliente);            
+            return Ok(cliente);
+            
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult PutCliente(Guid id)
+        {
+            Cliente cliente = _clienteRepository.GetCliente(id);
+            return Ok(cliente);
+        }
+            
+
+    }
+}
