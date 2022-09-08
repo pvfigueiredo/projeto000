027981e4e8843c1a7b4165f6e36872cfefdac795
Arthur de Sousa Lima <106205450+Arthurlima100@users.noreply.github.com>
2022-09-08 18:18:18 -0300

    Entities e erpository User

---
+++ Webservice/WebService/Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Webservice.Entities;
+using Webservice.Repositories;
+using WebService.Entities;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    [Route("clientes")]
+    public class UserController
+    {
+        UserRepository _userRepository;
+        public UserController()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetUser()
+        {
+            return _userRepository.GetUser();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Cliente> GetCliente(Guid id)
+        {
+            var user = _userRepository.GetUser(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        private ActionResult<Cliente> NotFound()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult PostUser(User request)
+        {
+            if (request == null)
+            {
+                return BadRequest();
+            }
+            var user = new User() { UserId = Guid.NewGuid(), Nome = request.Nome,Email = request.Email, UserName = request.UserName, Senha = request.Senha };
+            _userRepository.SaveUser(user);
+            return Ok(user);
+
+        }
+
+        private ActionResult BadRequest()
+        {
+            throw new NotImplementedException();
+        }
+
+        private ActionResult Ok(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Webservice/WebService/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿namespace WebService.Entities
+{
+    public class User
+    {
+        public Guid UserId { get; init; }
+        public string? Nome { get; init; }
+        public string? Email { get; init; }
+        public string? UserName { get; init; }
+        public string? Senha { get; init; }
+
+        public User() { }
+        public User(Guid userId, string nome, string email, string userName, string senha)
+        {
+            this.UserId = userId;
+            this.Nome = nome;
+            this.Email = email;
+            this.UserName = userName;
+            this.Senha = senha;
+        }
+    }
+}
