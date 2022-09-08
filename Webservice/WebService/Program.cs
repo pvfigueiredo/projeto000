5979227a061e360f2e29ae8451a06152d3fdbf76
Arthur de Sousa Lima <106205450+Arthurlima100@users.noreply.github.com>
2022-09-08 20:02:05 -0300

    atualizado

--- Webservice/WebService/Program.cs
+++ Webservice/WebService/Program.cs
@@ -1,8 +1,14 @@
+using Webservice.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+//Registrando uma instância singleton para ClienteRepository
+builder.Services.AddSingleton<IClientesRepository, ClientesRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
